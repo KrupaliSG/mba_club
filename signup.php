ca80da4e2a85a9e0c575b88482ed19cce0a1bc71
MBA CLUB <upscexam2029@gmail.com>
2026-04-05 10:44:07 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -15,21 +15,20 @@ $check = "SELECT * FROM users WHERE email='$email'";
 $result = mysqli_query($conn, $check);
 
 if(mysqli_num_rows($result) > 0){
+
     echo "Email already exists!";
+
 } else {
 
     $sql = "INSERT INTO users (name, email, phone, password)
             VALUES ('$name', '$email', '$phone', '$hashed')";
 
-        if(mysqli_query($conn, $sql)){
+    if(mysqli_query($conn, $sql)){
         header("Location: index.html");
         exit();
-        }
-
-        header("Location: index.html");
-        exit();
-
     } else {
         echo "Error: " . mysqli_error($conn);
     }
+
+}
 ?>
